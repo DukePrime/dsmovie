f4e7348cc914058276ef07355162d7f2977fa9c9
EduardoNagabe <eduardonagabe@gmail.com>
2022-01-14 14:57:03 -0300

    link

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,12 +6,12 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/DukePrime"></a>
+                    <a href="https://github.com/DukePrime">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
                         <p className='dsmovie-contact-link'>/devsuperior</p>
                     </div>
-
+                    </a>
                 </div>
             </nav>
         </header>
